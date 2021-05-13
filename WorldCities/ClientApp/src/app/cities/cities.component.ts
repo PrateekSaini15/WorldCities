b207f97d098985e47fe055fd3005064723650e1b
prateek saini <prateeksaini15@yahoo.com>
2021-05-13 16:00:20 +0530

    1. Add pagination on client side.

--- WorldCities/ClientApp/src/app/cities/cities.component.ts
+++ WorldCities/ClientApp/src/app/cities/cities.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { MatTableDataSource } from "@angular/material/table";
+import { MatPaginator } from "@angular/material/paginator";
 
 import {City} from './city';
 
@@ -8,16 +10,19 @@ import {City} from './city';
   templateUrl: './cities.component.html',
   styleUrls: ['./cities.component.css']
 })
-export class CitiesComponent implements OnInit {
+export class CitiesComponent {
 
-  public cities: City[];
+  public cities: MatTableDataSource<City>;
   public displayedColumns: string[] = ['id', 'name', 'lat', 'lon'];
+
+  @ViewChild('paginator') paginator : MatPaginator;
 
   constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) { }
 
   ngOnInit(): void {
     this.http.get<City[]>("http://localhost:5000/api/cities").subscribe(result => {
-      this.cities = result;
+      this.cities = new MatTableDataSource<City>(result);
+      this.cities.paginator = this.paginator;
     }, error => console.error(error));
   }
 
