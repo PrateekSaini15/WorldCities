47329e7cbc7d232ff93627dacd1c06c7152d2eb3
prateek saini <prateeksaini15@yahoo.com>
2021-05-22 16:09:00 +0530

    1. Create UI to add and edit a Country. 2. Create API to add and edit a country.

---
+++ WorldCities/ClientApp/src/app/countries/country-edit.component.ts
@@ -0,0 +1,116 @@
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Component, Inject } from "@angular/core";
+import {
+  AbstractControl,
+  AsyncValidatorFn,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { Country } from "./country";
+
+@Component({
+  selector: "app-country-edit",
+  templateUrl: "./country-edit.component.html",
+  styleUrls: ["./country-edit.component.css"],
+})
+export class CountryEditComponent {
+  title: string;
+  form: FormGroup;
+  country: Country;
+  id?: number;
+
+  constructor(
+    private http: HttpClient,
+    @Inject("BASE_URL") private baseUrl: string,
+    private fb: FormBuilder,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {
+    this.loadData();
+  }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      name: ["", Validators.required, this.isDupeField("name")],
+      iso2: [
+        "",
+        [Validators.required, Validators.pattern("[a-zA-z]{2}")],
+        this.isDupeField("iso2"),
+      ],
+      iso3: [
+        "",
+        [Validators.required, Validators.pattern("[a-zA-z]{3}")],
+        this.isDupeField("iso3"),
+      ],
+    });
+    this.loadData();
+  }
+
+  loadData() {
+    this.id = +this.activatedRoute.snapshot.paramMap.get("id");
+    if (this.id) {
+      var url = "http://localhost:5000/" + "api/countries/" + this.id;
+
+      this.http.get<Country>(url).subscribe(
+        (result) => {
+          this.country = result;
+          this.title = "Edit - " + this.country.name;
+          this.form.patchValue(this.country);
+        },
+        (error) => console.error(error)
+      );
+    } else {
+      this.title = "Create a new Country";
+    }
+  }
+
+  onSubmit() {
+    var country = this.id ? this.country : <Country>{};
+
+    country.name = this.form.get("name").value;
+    country.iso2 = this.form.get("iso2").value;
+    country.iso3 = this.form.get("iso3").value;
+
+    if (this.id) {
+      var url = "http://localhost:5000/" + "api/countries" + this.country.id;
+      this.http.put<Country>(url, country).subscribe(
+        (result) => {
+          console.log("Country" + country.id + "has been updated.");
+          this.router.navigate(["/countries"]);
+        },
+        (error) => console.log(error)
+      );
+    } else {
+      var url = "http://localhost:5000" + "api/countries";
+
+      this.http.post<Country>(url, country).subscribe(
+        (result) => {
+          console.log("Country" + result.id + "has been created");
+          this.router.navigate(["/countries"]);
+        },
+        (error) => console.error(error)
+      );
+    }
+  }
+
+  isDupeField(fieldName: string): AsyncValidatorFn {
+    return (
+      control: AbstractControl
+    ): Observable<{ [key: string]: any } | null> => {
+      var params = new HttpParams()
+        .set("countryId", this.id ? this.id.toString() : "0")
+        .set("fieldName", fieldName)
+        .set("fieldValue", control.value);
+      var url = "http//localhost:5000/" + "api/countries/IsDupeField";
+      return this.http.post<boolean>(url, null, { params }).pipe(
+        map((result) => {
+          return result ? { isDupeField: true } : null;
+        })
+      );
+    };
+  }
+}
--- WorldCities/Controllers/CountriesController.cs
+++ WorldCities/Controllers/CountriesController.cs
@@ -1,5 +1,7 @@
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WorldCities.Data;
 using WorldCities.Data.Models;
 
@@ -35,6 +37,78 @@ namespace WorldCities.Controllers
                 filterColumn,
                 filterQuery
             );
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ApiResult<Country>>> PostCountry(Country country)
+        {
+            _context.Countries.Add(country);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCity", new { id = country.Id }, country);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Country>> GetCountry(int id)
+        {
+            Country country = await _context.Countries.FindAsync(id);
+
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return country;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCountry(int id, Country country)
+        {
+            if (id != country.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(country).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CountryExist(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+        [HttpPost]
+        [Route("IsDupeField")]
+        public bool IsDupeField(int countryId, string fieldName, string fieldValue)
+        {
+            switch (fieldName)
+            {
+                case "name":
+                    return _context.Countries.Any(c => c.Name == fieldValue && c.Id != countryId);
+                case "iso2":
+                    return _context.Countries.Any(c => c.ISO2 == fieldValue && c.Id != countryId);
+                case "iso3":
+                    return _context.Countries.Any(c => c.ISO3 == fieldValue && c.Id != countryId);
+                default:
+                    return false;
+            }
+        }
+
+        private bool CountryExist(int id)
+        {
+            return _context.Countries.Any(e => e.Id == id);
         }
     }
 }
