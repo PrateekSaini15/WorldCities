22209b405effd949660980f9aec90dfac03c1fe0
prateek saini <prateeksaini15@yahoo.com>
2021-05-25 18:09:10 +0530

    1. Show country name for each city in city listing UI.

--- WorldCities/ClientApp/src/app/cities/cities.component.ts
+++ WorldCities/ClientApp/src/app/cities/cities.component.ts
@@ -1,20 +1,25 @@
-import { Component, OnInit, Inject, ViewChild } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { Component, OnInit, Inject, ViewChild } from "@angular/core";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { MatTableDataSource } from "@angular/material/table";
 import { MatPaginator, PageEvent } from "@angular/material/paginator";
 import { MatSort } from "@angular/material/sort";
 
-import {City} from './city';
+import { City } from "./city";
 
 @Component({
-  selector: 'app-cities',
-  templateUrl: './cities.component.html',
-  styleUrls: ['./cities.component.css']
+  selector: "app-cities",
+  templateUrl: "./cities.component.html",
+  styleUrls: ["./cities.component.css"],
 })
 export class CitiesComponent {
-
   public cities: MatTableDataSource<City>;
-  public displayedColumns: string[] = ['id', 'name', 'lat', 'lon'];
+  public displayedColumns: string[] = [
+    "id",
+    "name",
+    "lat",
+    "lon",
+    "countryName",
+  ];
 
   defaultPageIndex: number = 0;
   defaultPageSize: number = 10;
@@ -23,46 +28,53 @@ export class CitiesComponent {
   defaultFilterColumn: string = "name";
   filterQuery: string = null;
 
-  @ViewChild('paginator') paginator : MatPaginator;
+  @ViewChild("paginator") paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
-  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) { }
+  constructor(
+    private http: HttpClient,
+    @Inject("BASE_URL") private baseUrl: string
+  ) {}
 
   ngOnInit(): void {
-    this.loadData(null); 
+    this.loadData(null);
   }
 
-  loadData(query: string = null){
+  loadData(query: string = null) {
     var pageEvent = new PageEvent();
     pageEvent.pageIndex = this.defaultPageIndex;
     pageEvent.pageSize = this.defaultPageSize;
-    if (query){
+    if (query) {
       this.filterQuery = query;
     }
     this.getData(pageEvent);
   }
 
-  getData(event: PageEvent){
+  getData(event: PageEvent) {
     var url = "http://localhost:5000/" + "api/Cities";
     var params = new HttpParams()
       .set("pageIndex", event.pageIndex.toString())
       .set("pageSize", event.pageSize.toString())
-      .set("sortColumn", (this.sort) ? this.sort.active : this.defaultSortColumn)
-      .set("sortOrder", (this.sort) ? this.sort.direction : this.defaultSortOrder);
+      .set("sortColumn", this.sort ? this.sort.active : this.defaultSortColumn)
+      .set(
+        "sortOrder",
+        this.sort ? this.sort.direction : this.defaultSortOrder
+      );
 
-      if (this.filterQuery){
-        params = params
-          .set("filterColumn", this.defaultFilterColumn)
-          .set("filterQuery", this.filterQuery);
-      }
+    if (this.filterQuery) {
+      params = params
+        .set("filterColumn", this.defaultFilterColumn)
+        .set("filterQuery", this.filterQuery);
+    }
 
-    this.http.get<any>(url, {params})
-      .subscribe(result => {
+    this.http.get<any>(url, { params }).subscribe(
+      (result) => {
         this.paginator.length = result.totalCount;
         this.paginator.pageSize = result.pageSize;
         this.paginator.pageIndex = result.pageIndex;
         this.cities = new MatTableDataSource<City>(result.data);
-      }, error => console.error(error));
+      },
+      (error) => console.error(error)
+    );
   }
-
 }
